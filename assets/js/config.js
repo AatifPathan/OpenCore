f594e0ec28b3258177a6a45641670ad8e95eb1f4
Aatif Pathan <aatifpathan2004@gmail.com>
2026-07-06 11:33:49 +0530

    Update OPENCORE_SF_URL to use sessionStorage

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,3 @@
-window.OPENCORE_SF_URL = "https://orgfarm-64f194e55b-dev-ed.develop.my.salesforce.com";
+window.OPENCORE_SF_URL =
+    sessionStorage.getItem("oc_sf_instance_url") ||
+    "https://orgfarm-64f194e55b-dev-ed.develop.my.salesforce.com";
